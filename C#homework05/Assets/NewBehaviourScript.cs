553d03641042c18ec4ae9e44459db86248960c10
Windsword101 <63327487+Windsword101@users.noreply.github.com>
2020-06-10 09:04:20 +0800

    webgl

---
+++ C#homework05/Assets/NewBehaviourScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public SpriteRenderer kid;
+
+    public Rigidbody ball;
+    private void Start()
+    {
+        //練習一:靜態屬性
+        Time.timeScale = 2;
+        //練習二:非靜態屬性
+        kid.flipX = true;
+        //練習三
+        print(Mathf.Floor(1.234567f));
+        //練習四
+        ball.AddForce(0, 800, 1200);
+        ball.AddExplosionForce (50,new Vector3(0, 0, 0),50, 3.0F);
+
+
+
+
+    }
+    
+}
